8286c8281f8bd197806cb85ee184032d4fdb4319
newage <vadim.leontiev@gmail.com>
2013-09-23 21:48:48 +0300

    Feature (application); added user entity

--- install.sh
+++ install.sh
@@ -4,61 +4,10 @@ echo "Begin upload dependencies"
 curl -s http://getcomposer.org/installer | php
 php composer.phar install
 
-echo "Begin setup project"
-if [ ! -d "data" ]; then
-    mkdir --mode=0777 data
-fi
+echo "Copy config file for developer tools"
+cp vendor/zendframework/zend-developer-tools/config/zenddevelopertools.local.php.dist config/autoload/zdt.local.php
 
-cd data
-
-if [ ! -d "cache" ]; then
-    mkdir --mode=0777 cache
-fi
-
-if [ ! -d "logs" ]; then
-    mkdir --mode=0777 logs
-fi
-
-if [ ! -d "docs" ]; then
-    mkdir --mode=0777 docs
-fi
-
-if [ ! -d "indexes" ]; then
-    mkdir --mode=0777 indexes
-fi
-
-if [ ! -d "tmp" ]; then
-    mkdir --mode=0777 tmp
-fi
-
-if [ ! -d "upload" ]; then
-    mkdir --mode=0777 upload
-fi
-
-cd ../application/configs
-
-if [ ! -f "application.development.ini" ]; then
-    cp application.development.ini.dist application.development.ini
-fi
-
-cd ../../
-
-function zfsetup {
-    TFILE=".zf.ini"
-    echo "php.include_path = \"${PWD}/vendor/zend/zf1/library\"" > $TFILE
-    echo 'export PATH=$PATH:'${PWD} >> ~/.bashrc
-}
-
-function setupmanifest {
-    ./zf enable config.manifest Manifest
-}
-
-if [ ! -f "${PWD}/.zf.ini" ]; then
-    echo "Setup zf tool"
-    zfsetup
-    echo "Create new config file"
-    setupmanifest
-    echo "Enable Manifest"
-fi
+echo "Copy doctrine config"
+cp config/autoload/doctrine.local.php.dist config/autoload/doctrine.local.php
 
 echo "Install finish successfully!"
