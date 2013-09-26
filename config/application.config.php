3fe880ad9568de230f0a428b9f4529c32f8f3fb3
newage <vadim.leontiev@gmail.com>
2013-09-26 21:30:47 +0300

    Updated (core); added database profiler

--- config/application.config.php
+++ config/application.config.php
@@ -3,8 +3,6 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'ZendDeveloperTools',
-        'DoctrineModule',
-        'DoctrineORMModule',
         'Application',
     ),
 
