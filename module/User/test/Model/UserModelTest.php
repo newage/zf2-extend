fd53bb2e3cf3838a6b1c3aa54f22d470f4aaeecf
newage <vadim.leontiev@gmail.com>
2015-03-28 22:44:15 +0200

    Setup environment for tests

---
+++ module/User/test/Model/UserModelTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test\Model;
+
+use PHPUnit_Framework_TestCase;
+
+class UserModelTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testRegistration()
+    {
+
+    }
+}
