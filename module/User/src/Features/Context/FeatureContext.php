b145a53cd49fb0ca177de008db2bf50c4c4937a2
newage <vadim.leontiev@gmail.com>
2015-04-23 21:00:46 +0300

    Updated Context and added path to behat config

--- module/User/src/Features/Context/FeatureContext.php
+++ module/User/src/Features/Context/FeatureContext.php
@@ -1,23 +1,12 @@
 <?php
+namespace User\Features\Context;
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use Behat\MinkExtension\Context\MinkContext;
 
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
+class FeatureContext extends MinkContext
 {
     /**
      * Initializes context.
@@ -29,16 +18,4 @@ class FeatureContext extends BehatContext
     {
         // Initialize your context here
     }
-
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
 }
