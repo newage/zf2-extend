acbbc9f42f59c0550bb66afe56be7f99fcb68b5b
newage <vadim.leontiev@gmail.com>
2015-05-17 15:54:26 +0300

    [#21] Returned old file

--- module/User/bundle/behat/LoginContext.php
+++ module/User/bundle/behat/LoginContext.php
@@ -5,11 +5,12 @@ namespace User\Behat;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Gherkin\Node\TableNode;
+use Core\Behat\ExtendContext;
 
 /**
  * Features context.
  */
-class LoginContext implements SnippetAcceptingContext
+class LoginContext extends ExtendContext implements SnippetAcceptingContext
 {
 
     /**
